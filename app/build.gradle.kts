185b289866ca8a10be3451daf0dbd03319a40479
VHollund <vetle.hollund@nav.no>
2024-12-20 11:01:14 +0100

    lagt til logback classic og øker loglevel på kafka til warn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
 
     implementation("io.micrometer:micrometer-registry-prometheus:1.14.1")
 
+
+    implementation("ch.qos.logback:logback-classic:1.5.12")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:8.0")
 
     testImplementation(kotlin("test"))
