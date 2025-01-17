567615ad4083f65adfeb1360adec8d8efa50f146
VHollund <vetle.hollund@nav.no>
2025-01-17 09:23:05 +0100

    byttet type på azureConfig til å bruke riktig klasse og sende inn i commmonKtorModule

--- app/main/proxy/App.kt
+++ app/main/proxy/App.kt
@@ -39,7 +39,7 @@ fun Application.server(
 
     commonKtorModule(
         prometheus = prometheus,
-        azureConfig = null,
+        azureConfig = config.azure,
         infoModel = InfoModel(
             title = "AAP Arena HendelseProxy",
         ),
--- app/main/proxy/Config.kt
+++ app/main/proxy/Config.kt
@@ -1,16 +1,9 @@
 package proxy
 
 import libs.kafka.KafkaConfig
+import no.nav.aap.komponenter.httpklient.httpclient.tokenprovider.azurecc.AzureConfig
 
 private fun getEnvVar(envar: String) = System.getenv(envar) ?: error("missing envvar $envar")
-
-data class AzureConfig(
-    val tokenEndpoint: String = getEnvVar("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
-    val clientId: String = getEnvVar("AZURE_APP_CLIENT_ID"),
-    val clientSecret: String = getEnvVar("AZURE_APP_CLIENT_SECRET"),
-    val jwksUri: String = getEnvVar("AZURE_OPENID_CONFIG_JWKS_URI"),
-    val issuer: String = getEnvVar("AZURE_OPENID_CONFIG_ISSUER")
-)
 
 
 data class Config(
