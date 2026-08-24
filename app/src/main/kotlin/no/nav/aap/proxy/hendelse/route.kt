9eec4e339a15cfeb23a04fdb2974b6b93974b7e1
Fredrik Meyer <fredrik.meyer@nav.no>
2026-08-24 13:46:49 +0200

    AAP-BAU Også logg i dev mottatte hendelser

--- app/src/main/kotlin/no/nav/aap/proxy/hendelse/route.kt
+++ app/src/main/kotlin/no/nav/aap/proxy/hendelse/route.kt
@@ -5,15 +5,23 @@ import com.papsign.ktor.openapigen.route.path.normal.post
 import com.papsign.ktor.openapigen.route.route
 import com.papsign.ktor.openapigen.route.status
 import io.ktor.http.*
+import no.nav.aap.komponenter.miljo.Miljø
 import no.nav.aap.proxy.kafka.HendelseInput
 import no.nav.aap.proxy.kafka.HendelseInputFlereTpNr
 import no.nav.aap.proxy.kafka.HendelseProducer
+import org.slf4j.LoggerFactory
+
+private val log = LoggerFactory.getLogger("SamHendelse")
 
 fun NormalOpenAPIRoute.hendelse(
     hendelseApiProducer: HendelseProducer
 ) {
     route("/hendelse").status(202) {
         post<Unit, String, HendelseInputFlereTpNr> { _, input ->
+
+            if (!Miljø.erProd()) {
+                log.info("Hendelse mottatt: $input.")
+            }
 
             input.tpNr.forEach { tpNr ->
                 hendelseApiProducer.produce(
